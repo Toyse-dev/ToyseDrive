96c515c3d7c199272aff4c9711856b80e5676a28
Toyse_Devs <olayonwatoyib05@gmail.com>
2026-09-15 22:36:30 +0100

    Adding Read file class

--- fileUtil.h
+++ fileUtil.h
@@ -2,11 +2,31 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <iterator>
 
-std::string readBinary(const std::string& path) {
-    std::ifstream file(path, std::ios::binary);
+class ReadFile {
+    private:
+        std::string path;
+    public:
+        ReadFile(const std::string& p) : path(p) {}
+
+        std::string readBinary() {
+    std::ifstream file(path, std::ios::binary | std::ios::ate);
     if (!file.is_open()) {
         std::cerr << "Error opening file: " << path << std::endl;
         return "";
     }
+
+    std::string rawBytes{
+        std::istreambuf_iterator<char>(file),
+        std::istreambuf_iterator<char>()
+    };
+
+    std::cout << "Read " << rawBytes.size() << " bytes from file: " << path << std::endl;
+    return rawBytes;
+}
+};
+
+class WriteFile {
+    private:
 }
